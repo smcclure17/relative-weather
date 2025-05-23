890d3b794f2dbba4c2e0866a3b8fe18d3cf0c9f4
Sean McClure <sean.mcclure17@gmail.com>
2025-05-23 16:45:58 -0400

    og stuff

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,34 @@
 import { HomePage } from "@/screens/homepage";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <HomePage/>
-  )
+    <>
+      <Head>
+        <title>releweather | weather compared to yesterday</title>
+        <meta
+          name="description"
+          content="Compare today's weather to yesterday's"
+        />
+
+        <meta property="og:title" content="releweather" />
+        <meta
+          property="og:description"
+          content="Compare today's weather to yesterday's."
+        />
+        <meta property="og:image" content="/og-image.png" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://relaweather.io/" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="releweather" />
+        <meta
+          name="twitter:description"
+          content="Compare today's weather to yesterday's."
+        />
+        <meta name="twitter:image" content="/og-image.png" />
+      </Head>
+      <HomePage />
+    </>
+  );
 }
